a2633d87dcd239898a77ea828f91f94a27cc8ad5
sharafgit <79160876+sharafgit@users.noreply.github.com>
2021-11-14 00:17:56 +0200

    Add DataTable Files For DataTable Controller in Employee

---
+++ EmplSchoolMange/BL/Service/EmployeeServiceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmplSchoolMange.BL.Service
+{
+    public class EmployeeServiceController : Controller
+    {
+        [Route("/EmployeeService/GitCity")]
+        public IActionResult GitCity()
+        {
+            return Json("");
+        }
+    }
+}
